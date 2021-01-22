7582245410130893fa9e6747ef062e36abbcd697
Derotino Silveira <silveirado@gmail.com>
2021-01-22 14:03:33 -0300

    Some fixes and update dependencies versions

--- rocketchat-out.js
+++ rocketchat-out.js
@@ -1,100 +1,118 @@
-const api = require('./rocketchat');
+const api = require("./rocketchat");
 
-module.exports = function(RED) {
-	'use strict';
+module.exports = function (RED) {
+  "use strict";
 
-	function RocketChatOut(config) {
-		RED.nodes.createNode(this, config);
-		const node = this;
-		node.server = RED.nodes.getNode(config.server);
+  function RocketChatOut(config) {
+    RED.nodes.createNode(this, config);
+    const node = this;
+    node.server = RED.nodes.getNode(config.server);
 
-		const {
-			messageText,
-			messageTextType,
-			avatarText,
-			avatarTextType,
-			aliasText,
-			aliasTextType,
-			emojiText,
-			emojiTextType,
-			attachments: configAttachments,
-			attachmentsType,
-			room,
-			roomType,
-			roomData
-		} = config;
+    const {
+      messageText,
+      messageTextType,
+      avatarText,
+      avatarTextType,
+      aliasText,
+      aliasTextType,
+      emojiText,
+      emojiTextType,
+      attachments: configAttachments,
+      attachmentsType,
+      room,
+      roomType,
+    } = config;
 
-		node.on('input', async function(msg) {
-			const { host, user, token } = node.server;
+    node.on("input", async function (msg) {
+      const { host, user, token } = node.server;
 
-			const apiInstance = api({ host, user, token });
+      const apiInstance = api({ host, user, token });
 
-			let roomId;
-			if (roomType === 'form') {
-				const { i } = JSON.parse(roomData);
-				roomId = i;
-			} else {
-				roomId = RED.util.evaluateNodeProperty(room, roomType, this, msg);
-			}
+      let roomId;
+      try {
+        if (roomType === "form") {
+          const { i } = JSON.parse(room);
+          roomId = i;
+        } else {
+          roomId = RED.util.evaluateNodeProperty(room, roomType, this, msg);
+        }
+      } catch (error) {
+        node.error(RED._("rocketchat-out.errors.error-processing", error));
+        node.status({
+          fill: "red",
+          shape: "ring",
+          text: RED._("rocketchat-out.errors.error-processing", error),
+        });
+        return;
+      }
 
-			const avatar = RED.util.evaluateNodeProperty(avatarText, avatarTextType, this, msg);
-			const alias = RED.util.evaluateNodeProperty(aliasText, aliasTextType, this, msg);
-			const emoji = RED.util.evaluateNodeProperty(emojiText, emojiTextType, this, msg);
-			const text = RED.util.evaluateNodeProperty(messageText, messageTextType, this, msg);
-			const attachments = RED.util.evaluateNodeProperty(configAttachments, attachmentsType, this, msg);
+      const avatar = RED.util.evaluateNodeProperty(avatarText, avatarTextType, this, msg);
+      const alias = RED.util.evaluateNodeProperty(aliasText, aliasTextType, this, msg);
+      const emoji = RED.util.evaluateNodeProperty(emojiText, emojiTextType, this, msg);
+      const text = RED.util.evaluateNodeProperty(messageText, messageTextType, this, msg);
+      const attachments = RED.util.evaluateNodeProperty(configAttachments, attachmentsType, this, msg);
 
-			if (roomId == null) {
-				node.warn(RED._('rocketchat-out.errors.invalid-data'));
-				node.status({ fill: 'red', shape: 'ring', text: 'rocketchat-out.errors.invalid-data' });
-				return;
-			}
+      if (roomId == null) {
+        node.warn(RED._("rocketchat-out.errors.invalid-data"));
+        node.status({ fill: "red", shape: "ring", text: "rocketchat-out.errors.invalid-data" });
+        return;
+      }
 
-			if (config.destination === 'users') {
-				node.status({ fill: 'blue', shape: 'dot', text: 'rocketchat-out.label.sending' });
-				try {
-					const { success, room, errors } = await apiInstance.createIM({ username: roomId });
-					if (success) {
-						const { _id } = room;
-						roomId = _id;
-					} else {
-						node.error(RED._('rocketchat-out.errors.error-processing', errors));
-						node.status({
-							fill: 'red',
-							shape: 'ring',
-							text: RED._('rocketchat-out.errors.error-processing', errors)
-						});
-					}
-				} catch (error) {
-					node.error(RED._('rocketchat-out.errors.error-processing', error));
-					node.status({
-						fill: 'red',
-						shape: 'ring',
-						text: RED._('rocketchat-out.errors.error-processing', error)
-					});
-				}
-			}
+      if (config.destination === "users") {
+        node.status({ fill: "blue", shape: "dot", text: "rocketchat-out.label.sending" });
+        try {
+          const { user, success: infoSuccess } = (await apiInstance.getUserInfo(roomId)) || { success: false };
 
-			node.status({ fill: 'blue', shape: 'dot', text: 'rocketchat-out.label.sending' });
-			try {
-				await apiInstance.send({
-					roomId,
-					text,
-					attachments,
-					alias,
-					avatar,
-					emoji
-				});
-			} catch (error) {
-				node.error(RED._('rocketchat-out.errors.error-processing', error));
-				node.status({
-					fill: 'red',
-					shape: 'ring',
-					text: RED._('rocketchat-out.errors.error-processing', error)
-				});
-			}
-			node.status({});
-		});
-	}
+          if (infoSuccess === false || user == null) {
+            node.error(RED._("rocketchat-out.errors.error-processing", errors));
+            node.status({ fill: "red", shape: "ring", text: "rocketchat-out.errors.invalid-data" });
+            return;
+          }
 
-	RED.nodes.registerType('rocketchat-out', RocketChatOut);
+          const { success, room, errors } = await apiInstance.createIM({ username: user.username });
+
+          if (success) {
+            const { _id } = room;
+            roomId = _id;
+          } else {
+            node.error(RED._("rocketchat-out.errors.error-processing", errors));
+            node.status({
+              fill: "red",
+              shape: "ring",
+              text: RED._("rocketchat-out.errors.error-processing", errors),
+            });
+          }
+        } catch (error) {
+          node.error(RED._("rocketchat-out.errors.error-processing", error));
+          node.status({
+            fill: "red",
+            shape: "ring",
+            text: RED._("rocketchat-out.errors.error-processing", error),
+          });
+        }
+      }
+
+      node.status({ fill: "blue", shape: "dot", text: "rocketchat-out.label.sending" });
+      try {
+        await apiInstance.send({
+          roomId,
+          text,
+          attachments,
+          alias,
+          avatar,
+          emoji,
+        });
+      } catch (error) {
+        node.error(RED._("rocketchat-out.errors.error-processing", error));
+        node.status({
+          fill: "red",
+          shape: "ring",
+          text: RED._("rocketchat-out.errors.error-processing", error),
+        });
+      }
+      node.status({});
+    });
+  }
+
+  RED.nodes.registerType("rocketchat-out", RocketChatOut);
 };
--- rocketchat.js
+++ rocketchat.js
@@ -1,134 +1,147 @@
-const axios = require('axios');
+const axios = require("axios");
 
 module.exports = ({ host, user, token }) => ({
-	async spotlight(query) {
-		const { data } = await axios.get(`${host}/api/v1/spotlight`, {
-			params: {
-				query
-			},
-			headers: {
-				'X-Auth-Token': token,
-				'X-User-Id': user
-			}
-		});
-		return data;
-	},
-	async createIM({ username }) {
-		const { data } = await axios.post(
-			`${host}/api/v1/im.create`,
-			{ username },
-			{
-				headers: {
-					'X-Auth-Token': token,
-					'X-User-Id': user
-				}
-			}
-		);
-		return data;
-	},
-	async send({ roomId, text, attachments, alias, avatar, emoji }) {
-		const { data } = await axios.post(
-			`${host}/api/v1/chat.postMessage`,
-			{ roomId, text, alias, emoji, avatar, attachments },
-			{
-				headers: {
-					'X-Auth-Token': token,
-					'X-User-Id': user
-				}
-			}
-		);
-		return data;
-	},
-	async getSubscriptions() {
-		const { data } = await axios.get(`${host}/api/v1/subscriptions.get`, {
-			headers: {
-				'X-Auth-Token': token,
-				'X-User-Id': user
-			}
-		});
-		return data;
-	},
-	async getSubscription({ roomId }) {
-		const { data } = await axios.get(`${host}/api/v1/subscriptions.getOne`, {
-			params: {
-				roomId
-			},
-			headers: {
-				'X-Auth-Token': token,
-				'X-User-Id': user
-			}
-		});
+  async spotlight(query) {
+    const { data } = await axios.get(`${host}/api/v1/spotlight`, {
+      params: {
+        query,
+      },
+      headers: {
+        "X-Auth-Token": token,
+        "X-User-Id": user,
+      },
+    });
+    return data;
+  },
+  async createIM({ username }) {
+    const { data } = await axios.post(
+      `${host}/api/v1/im.create`,
+      { username },
+      {
+        headers: {
+          "X-Auth-Token": token,
+          "X-User-Id": user,
+        },
+      }
+    );
+    return data;
+  },
+  async send({ roomId, text, attachments, alias, avatar, emoji }) {
+    const { data } = await axios.post(
+      `${host}/api/v1/chat.postMessage`,
+      { roomId, text, alias, emoji, avatar, attachments },
+      {
+        headers: {
+          "X-Auth-Token": token,
+          "X-User-Id": user,
+        },
+      }
+    );
+    return data;
+  },
+  async getSubscriptions() {
+    const { data } = await axios.get(`${host}/api/v1/subscriptions.get`, {
+      headers: {
+        "X-Auth-Token": token,
+        "X-User-Id": user,
+      },
+    });
+    return data;
+  },
+  async getSubscription({ roomId }) {
+    const { data } = await axios.get(`${host}/api/v1/subscriptions.getOne`, {
+      params: {
+        roomId,
+      },
+      headers: {
+        "X-Auth-Token": token,
+        "X-User-Id": user,
+      },
+    });
 
-		return data;
-	},
-	async getUnreadMessages({ roomId, oldest, type }) {
-		try {
-			let endpoint;
-			switch (type) {
-				case 'g':
-					endpoint = 'groups';
-					break;
-				case 'd':
-					endpoint = 'im';
-					break;
-				default:
-					endpoint = 'channels';
-					break;
-			}
+    return data;
+  },
+  async getUnreadMessages({ roomId, oldest, type }) {
+    try {
+      let endpoint;
+      switch (type) {
+        case "g":
+          endpoint = "groups";
+          break;
+        case "d":
+          endpoint = "im";
+          break;
+        default:
+          endpoint = "channels";
+          break;
+      }
 
-			const { data } = await axios.get(`${host}/api/v1/${endpoint}.history`, {
-				params: {
-					roomId,
-					oldest
-				},
-				headers: {
-					'X-Auth-Token': token,
-					'X-User-Id': user
-				}
-			});
-			return data;
-		} catch ({ message }) {
-			console.error(message);
-			return { success: false, error: { message } };
-		}
-	},
-	async markAsRead({ rid }) {
-		const { data } = await axios.post(
-			`${host}/api/v1/subscriptions.read`,
-			{ rid },
-			{
-				headers: {
-					'X-Auth-Token': token,
-					'X-User-Id': user
-				}
-			}
-		);
-		return data;
-	},
-	async createChannel({ name, members, readOnly = false }) {
-		const { data } = await axios.post(
-			`${host}/api/v1/channels.create`,
-			{ name, members, readOnly },
-			{
-				headers: {
-					'X-Auth-Token': token,
-					'X-User-Id': user
-				}
-			}
-		);
-		return data;
-	},
-	async createGroup({ name, members, readOnly = false }) {
-		const { data } = await axios.post(
-			`${host}/api/v1/groups.create`,
-			{ name, members, readOnly },
-			{
-				headers: {
-					'X-Auth-Token': token,
-					'X-User-Id': user
-				}
-			}
-		);
-		return data;
-	}
+      const { data } = await axios.get(`${host}/api/v1/${endpoint}.history`, {
+        params: {
+          roomId,
+          oldest,
+        },
+        headers: {
+          "X-Auth-Token": token,
+          "X-User-Id": user,
+        },
+      });
+      return data;
+    } catch ({ message }) {
+      console.error(message);
+      return { success: false, error: { message } };
+    }
+  },
+  async markAsRead({ rid }) {
+    const { data } = await axios.post(
+      `${host}/api/v1/subscriptions.read`,
+      { rid },
+      {
+        headers: {
+          "X-Auth-Token": token,
+          "X-User-Id": user,
+        },
+      }
+    );
+    return data;
+  },
+  async createChannel({ name, members, readOnly = false }) {
+    const { data } = await axios.post(
+      `${host}/api/v1/channels.create`,
+      { name, members, readOnly },
+      {
+        headers: {
+          "X-Auth-Token": token,
+          "X-User-Id": user,
+        },
+      }
+    );
+    return data;
+  },
+  async createGroup({ name, members, readOnly = false }) {
+    const { data } = await axios.post(
+      `${host}/api/v1/groups.create`,
+      { name, members, readOnly },
+      {
+        headers: {
+          "X-Auth-Token": token,
+          "X-User-Id": user,
+        },
+      }
+    );
+    return data;
+  },
+  async getUserInfo(userId) {
+    const { data } = await axios.get(`${host}/api/v1/users.info`, {
+      params: {
+        userId,
+      },
+      headers: {
+        "X-Auth-Token": token,
+        "X-User-Id": user,
+      },
+    });
+
+    return data;
+  },
 });
